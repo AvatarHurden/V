32dabc6f70aac830a55d9a4c4cd3e604500c730c
Arthur Vedana <agieselvedana@gmail.com>
2016-09-10 19:01:45 -0300

    Criei tentativa de definição da gramática da linguagem.
    Não sei como isso funciona, ou sequer se funciona. Do jeito que está, parece que vai ser possível definir tudo que precisa, mas não de uma maneira bonita. Boa sorte.

--- Interpretador L1/Program.fs
+++ Interpretador L1/Program.fs
@@ -1,6 +1,52 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+type Type =
+    | X of string
+    | Int
+    | Bool
+    | Function of Type * Type
+
+type n =
+    | N of int
+
+type b =
+    | True
+    | False
+    
+type op =
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
+    | LessThan
+    | LessOrEqual
+    | Equal
+    | Different
+    | GreaterOrEqual
+    | GreaterThan
+
+type Ident =
+    | X of string
+
+type term =
+    | N of n
+    | B of b
+    | OP of term * op * term
+    | Cond of term * term * term
+    | X of Ident
+    | App of term * term
+    | Fn of Ident * Type * term
+    | Let of Ident * Type * term * term
+    | LetRec of Type * Type * Ident * term * term
+    | Nil
+    | Cons of term * term
+    | IsEmpty of term
+    | Head of term
+    | Tail of term
+    | Raise
+    | Try of term * term
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
