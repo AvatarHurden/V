495ff01baf74f65b3acce626f9abd12bd68eb989
Leonardo Nahra <leonardonahra@gmail.com>
2016-09-15 00:20:13 -0300

    Adiciona regras para avaliador big step L1
    Adiciona regras de valor, operadores (com excessão do Divide) e
    condicional para avaliador operacional big step L1

--- Interpretador L1/Program.fs
+++ Interpretador L1/Program.fs
@@ -8,12 +8,16 @@ type Type =
     | Function of Type * Type
 
 type n =
-    | N of int
+    | I of int
 
 type b =
     | True
     | False
-    
+
+type v =
+    | B of b
+    | N of n
+
 type op =
     | Add
     | Subtract
@@ -30,8 +34,7 @@ type Ident =
     | X of string
 
 type term =
-    | N of n
-    | B of b
+    | V of v
     | OP of term * op * term
     | Cond of term * term * term
     | X of Ident
@@ -46,6 +49,87 @@ type term =
     | Tail of term
     | Raise
     | Try of term * term
+
+exception WrongExpression
+
+let rec eval t =
+    match t with
+    | V(v) -> V(v)
+    | OP(t1, Add, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) -> V(N(I(n1 + n2)))
+        | _, _ -> raise WrongExpression
+    | OP(t1, Subtract, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) -> V(N(I(n1 - n2)))
+        | _, _ -> raise WrongExpression
+    | OP(t1, Multiply, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) -> V(N(I(n1 * n2)))
+        | _, _ -> raise WrongExpression
+    | OP(t1, LessThan, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) when n1 < n2 -> V(B(True))
+        | V(N(I(n1))), V(N(I(n2))) when not(n1 < n2) -> V(B(False))
+        | _, _ -> raise WrongExpression
+    | OP(t1, LessOrEqual, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) when n1 <= n2 -> V(B(True))
+        | V(N(I(n1))), V(N(I(n2))) when not(n1 <= n2) -> V(B(False))
+        | _, _ -> raise WrongExpression
+    | OP(t1, Equal, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) when n1 = n2 -> V(B(True))
+        | V(N(I(n1))), V(N(I(n2))) when not(n1 = n2) -> V(B(False))
+        | _, _ -> raise WrongExpression
+    | OP(t1, Different, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) when n1 <> n2 -> V(B(True))
+        | V(N(I(n1))), V(N(I(n2))) when not(n1 <> n2) -> V(B(False))
+        | _, _ -> raise WrongExpression
+    | OP(t1, GreaterOrEqual, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) when n1 >= n2 -> V(B(True))
+        | V(N(I(n1))), V(N(I(n2))) when not(n1 >= n2) -> V(B(False))
+        | _, _ -> raise WrongExpression
+    | OP(t1, GreaterThan, t2) ->
+        let t1' = eval t1 in
+        let t2' = eval t2 in
+        match t1', t2' with
+        | V(N(I(n1))), V(N(I(n2))) when n1 > n2 -> V(B(True))
+        | V(N(I(n1))), V(N(I(n2))) when not(n1 > n2) -> V(B(False))
+        | _, _ -> raise WrongExpression
+    | Cond(t1, t2, t3) ->
+        let t1' = eval t1 in
+        match t1' with
+        | V(B(True)) ->
+            let t2' = eval t2 in
+            match t2' with
+            | V(v) -> V(v)
+            | _ -> raise WrongExpression
+        | V(B(False)) ->
+            let t3' = eval t3 in
+            match t3' with
+            | V(v) -> V(v)
+            | _ -> raise WrongExpression
+        | _ -> raise WrongExpression
+    | _ -> raise WrongExpression
 
 [<EntryPoint>]
 let main argv = 
