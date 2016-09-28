d1984a7988dc2e08e35a3dabf6aac74fd02c875d
Arthur Giesel Vedana <agieselvedana@gmail.com>
2016-09-28 13:14:19 -0300

    Aplicação agora é um tipo de operação

--- Interpretador L1/Tests.fs
+++ Interpretador L1/Tests.fs
@@ -15,7 +15,7 @@ type TestReplace() =
     [<Category("Value")>]
     [<Category("X")>]
     member that.``let``() =
-        let app = App(Fn("x", Int, OP(X("x"), Add, I(1))), OP(X("x"), Add, X("y")))
+        let app = OP(Fn("x", Int, OP(X("x"), Add, I(1))), Application, OP(X("x"), Add, X("y")))
         let defy = Let("y", Int, I(40), app)
         let defx = Let("x", Int, OP(I(10), Add, X("y")), defy)
 
@@ -29,13 +29,12 @@ type TestEval() =
     [<Test>]
     [<Category("LetRec")>]
     [<Category("Cond")>]
-    [<Category("App")>]
     [<Category("OP")>]
     [<Category("Value")>]
     [<Category("X")>]
     member that.``factorial``() =
-        let fatMult = OP(X("x"), Multiply, App(X("fat"), OP(X("x"), Subtract, I(1))))
+        let fatMult = OP(X("x"), Multiply, OP(X("fat"), Application, OP(X("x"), Subtract, I(1))))
         let fnTerm =  Cond(OP(X("x"), Equal, I(0)), I(1), fatMult)
-        let fat = LetRec("fat", Int, Int, "x", fnTerm, App(X("fat"), I(5))) in
+        let fat = LetRec("fat", Int, Int, "x", fnTerm, OP(X("fat"), Application, I(5))) in
 
         eval fat |> should equal (I(120))
