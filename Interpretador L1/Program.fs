3e3132334b49c9e302e7b9430cda494b0872b6a1
Arthur Vedana <agieselvedana@gmail.com>
2016-09-30 15:54:59 -0300

    É possível parsear e avaliar qualquer texto
    O programa recebe como parâmetro ou um nome de arquivo ou um texto qualquer para parsear, e então imprime a sua análise e resultado. Caso não seja passado nenhum parâmetro, é possível inserir o texto (ou nome do arquivo) diretamente na sua execução

--- Interpretador L1/Program.fs
+++ Interpretador L1/Program.fs
@@ -1,12 +1,41 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
 open Definition
+open Sugar
 open Evaluation
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    
+    let file = 
+        if argv.Length = 0 then
+            let mutable cur = System.Console.ReadLine()
+            let mutable acc = cur
+            while cur.Length > 0 do
+                cur <- System.Console.ReadLine()
+                if cur.Length > 0 then
+                    acc <- acc + "\n" + cur
+            acc
+        else
+            argv.[0]
+
+    let text = 
+        if IO.File.Exists(file) then
+            file |> IO.File.ReadAllText
+        else
+            file
+
+    try
+        let term = parseTerm text
+            
+        term |> print |> printfn "Your input was interpreted as:\n\n%O"
+        Console.WriteLine()
+        Console.WriteLine()
+        term |> eval |> print |> printfn "Your program resulted in:\n\n%O"
+    with
+    | InvalidEntryText t -> Console.WriteLine t
 
     let t2 = System.Console.ReadLine()
     0 // return an integer exit code
