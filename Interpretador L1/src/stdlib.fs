f646141b36655cdcba65fb40230c8660c1981097
Arthur Giesel Vedana <agieselvedana@gmail.com>
2016-10-21 13:00:27 -0200

    Adicionado exists, indexOf para stdlib, criados testes

--- Interpretador L1/src/stdlib.fs
+++ Interpretador L1/src/stdlib.fs
@@ -1,6 +1,11 @@
 ﻿module stdlib
 
 let content = """
+
+// ====================
+// Arithmetic functions
+// ====================
+
 let rec remainder(x, y) {
     if y = 0 then  
         raise
@@ -9,9 +14,11 @@ let rec remainder(x, y) {
     else
         remainder (x-y) y
 };
+
 let negate(x) {
 	0-x
 };
+
 let abs(x) {
 	if x < 0 then
 		negate x
@@ -19,24 +26,31 @@ let abs(x) {
 		x
 };
 
+// =================
+// Logical functions
+// =================
+
 let not(t) {
 	if t then
 		false
 	else
 		true
 };
+
 let and(t1, t2) {
 	if t1 then
 		t2
 	else
 		false
 };
+
 let or(t1, t2) {
 	if t1 then
 		true
 	else
 		t2
 };
+
 let xor(t1, t2) {
 	if t1 then
 		not t2
@@ -44,24 +58,22 @@ let xor(t1, t2) {
 		t2
 };
 
-let rec length(ls) {
+// ====================
+// Basic List functions
+// ====================
+
+let rec append(x, ls) {
 	if empty? ls then
-		0
+		x::ls
 	else
-		1 + length (tail ls)
+		(head ls)::(append x (tail ls))
 };
-let reverse(ls) {
-	if empty? ls then
-		nil
+
+let rec concat(ls1, ls2) {
+	if empty? ls1 then
+		ls2
 	else
-		let rec f(lsOld, lsNew) {
-			let new = (head lsOld)::(lsNew);
-			if empty? (tail lsOld) then
-				new
-			else
-				f (tail lsOld) new
-		};
-		f ls nil
+		(head ls1)::(concat (tail ls1) ls2)
 };
 
 let rec last(ls) {
@@ -80,17 +92,43 @@ let rec init(ls){
 	else
 		(head ls)::(init (tail ls))
 };
-let rec append(x, ls) {
+let rec length(ls) {
 	if empty? ls then
-		x::ls
+		0
 	else
-		(head ls)::(append x (tail ls))
+		1 + length (tail ls)
 };
-let rec concat(ls1, ls2) {
-	if empty? ls1 then
-		ls2
+
+
+// =========================
+// List generation functions
+// =========================
+
+let rec range(start, finish, inc) {
+	if ((start > finish && inc > 0) || (start < finish && inc < 0)) then
+		nil
+	else if (inc > 0 && start <= finish) || (inc < 0 && start >= finish) then
+		start::(range (start+inc) finish inc)
 	else
-		(head ls1)::(concat (tail ls1) ls2)
+		nil
+};
+
+// =============================
+// List transformation functions
+// =============================
+
+let reverse(ls) {
+	if empty? ls then
+		nil
+	else
+		let rec f(lsOld, lsNew) {
+			let new = (head lsOld)::(lsNew);
+			if empty? (tail lsOld) then
+				new
+			else
+				f (tail lsOld) new
+		};
+		f ls nil
 };
 
 let rec map(f, ls) {
@@ -99,12 +137,19 @@ let rec map(f, ls) {
     else
         (f (head ls))::(map f (tail ls))
 };
+
+
+// ========================
+// List reduction functions
+// ========================
+
 let rec fold(f, acc, ls) {
     if empty? ls then
         acc
     else
         fold f (f acc (head ls)) (tail ls)
 };
+
 let rec reduce(f, ls) {
     if empty? ls then
         raise
@@ -112,60 +157,6 @@ let rec reduce(f, ls) {
         head ls
     else
         f (head ls) (reduce f (tail ls))
-};
-
-let rec take(x, ls) {
-	if (x = 0) || (empty? ls) then
-		nil
-	else
-		(head ls)::((tail ls) |> take (x-1))
-};
-let rec drop(x, ls) {
-    if x > 0 && empty? ls then
-        []
-    else if x = 0 then
-        ls
-    else
-        drop (x-1) (tail ls)
-};
-let rec takeWhile(pred, ls) {
-	if empty? ls then
-		nil
-	else if (head ls) |> pred |> not then
-		nil
-	else
-		(head ls)::(tail ls |> takeWhile pred)
-};
-let rec dropWhile(pred, ls) {
-    if empty? ls then
-        []
-    else if head ls |> pred |> not then
-        ls
-    else
-        dropWhile pred (tail ls)
-};
-let rec filter(pred, ls) {
-	if empty? ls then
-		nil
-	else if head ls |> pred then
-		head ls::tail ls |> filter pred
-	else
-		tail ls |> filter pred
-};
-
-let rec nth(index, ls) {
-    if index > 0 && empty? ls then 
-        raise
-    else if index = 0 then
-        head ls
-    else
-        nth (index - 1) (tail ls)
-};
-let subList(start, end, ls) {
-    if start < 0 || end < 0 || end < start || end > length ls then
-        raise
-    else
-        take (end-start) <| drop start ls 
 };
 
 let rec all(pred, ls) {
@@ -176,6 +167,7 @@ let rec all(pred, ls) {
 	else
 		(tail ls) |> all pred
 };
+
 let rec any(pred, ls) {
 	if empty? ls then
 		false
@@ -185,12 +177,97 @@ let rec any(pred, ls) {
 		(tail ls) |> any pred
 };
 
-let rec range(start, finish, inc) {
-	if ((start > finish && inc > 0) || (start < finish && inc < 0)) then
+// =================
+// Sublist functions
+// =================
+
+let rec take(x, ls) {
+	if (x = 0) || (empty? ls) then
 		nil
-	else if (inc > 0 && start <= finish) || (inc < 0 && start >= finish) then
-		start::(range (start+inc) finish inc)
 	else
-		nil
+		(head ls)::((tail ls) |> take (x-1))
 };
+
+let rec drop(x, ls) {
+    if x > 0 && empty? ls then
+        []
+    else if x = 0 then
+        ls
+    else
+        drop (x-1) (tail ls)
+};
+
+let rec takeWhile(pred, ls) {
+	if empty? ls then
+		nil
+	else if (head ls) |> pred |> not then
+		nil
+	else
+		(head ls)::(tail ls |> takeWhile pred)
+};
+
+let rec dropWhile(pred, ls) {
+    if empty? ls then
+        []
+    else if head ls |> pred |> not then
+        ls
+    else
+        dropWhile pred (tail ls)
+};
+
+let subList(start, end, ls) {
+    if start < 0 || end < 0 || end < start || end > length ls then
+        raise
+    else
+        take (end-start) <| drop start ls 
+};
+
+// =====================
+// List search functions
+// =====================
+
+let rec exists(t, ls) {
+    if empty? ls then
+        false
+    else if t = (head ls) then
+        true
+    else
+        exists t <| tail ls
+};      
+
+let rec filter(pred, ls) {
+	if empty? ls then
+		nil
+	else if head ls |> pred then
+		head ls::tail ls |> filter pred
+	else
+		tail ls |> filter pred
+};
+
+// =======================
+// List indexing functions
+// =======================
+
+let indexOf(t, ls) {
+    let rec f(index, ls) {
+	    if empty? ls then
+		    -1
+	    else if t = (head ls) then
+		    index
+        else
+            f (index+1) (tail ls)
+	};
+    f 0 ls
+};
+
+let rec nth(index, ls) {
+    if index > 0 && empty? ls then 
+        raise
+    else if index = 0 then
+        head ls
+    else
+        nth (index - 1) (tail ls)
+};
+
+
 """
---
+++ Interpretador L1/tests/stdlibTests.fs
@@ -0,0 +1,43 @@
+﻿module stdlibTests
+
+open NUnit.Framework
+open FsUnit
+open Definition
+open Evaluation
+open TypeInference
+open Parser
+
+let compare (text, term) =
+    let parsed = parseTerm text (List.empty)
+    let typ = typeInfer <| parsed
+    let evaluated = evaluate <| parsed
+    evaluated |> should equal term
+
+[<TestFixture>]
+type Teststdlib() =
+
+    [<Test>]
+    member that.existsTrue() =
+        compare ("exists [5] [[1],[2],[],[5]]", True)
+
+    [<Test>]
+    member that.existsFalse() =
+        compare ("exists false [true, true, true]", False)
+
+    [<Test>]
+    member that.existsWrong() =
+        (fun () -> compare ("exists (\x => x) []", True) |> ignore)
+             |> should throw typeof<InvalidType>
+
+    [<Test>]
+    member that.indexOfPositive() =
+        compare ("indexOf [5] [[1],[2],[],[5]]", I 3)
+
+    [<Test>]
+    member that.indexOfNegative() =
+        compare ("indexOf false [true, true, true]", I -1)
+
+    [<Test>]
+    member that.indexOfWrong() =
+        (fun () -> compare ("indexOf 4 [true]", Bool) |> ignore)
+             |> should throw typeof<InvalidType>
