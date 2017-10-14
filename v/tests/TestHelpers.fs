64742841e30cf8737cfa4bf972259d42e101e979
Arthur Giesel Vedana <agieselvedana@gmail.com>
2017-10-14 10:54:24 -0300

    Wrote basic tests for generating match expressions in translation

---
+++ v/tests/TestHelpers.fs
@@ -0,0 +1,16 @@
+﻿module TestHelpers
+
+open NUnit.Framework
+open FsUnit
+open Parser
+open Definition
+
+let ExInt = ExConstType (Definition.Int, [])
+let ExBool = ExConstType (Definition.Bool, [])
+let ExChar = ExConstType (Definition.Char, [])
+let ExList x = ExConstType (Definition.List, [x])
+
+let Int = ConstType (Definition.Int, [])
+let Bool = ConstType (Definition.Bool, [])
+let Char = ConstType (Definition.Char, [])
+let List x = ConstType (Definition.List, [x])
---
+++ v/tests/TranslationTests.fs
@@ -0,0 +1,57 @@
+﻿module TranslationTests
+
+open NUnit.Framework
+open FsUnit
+open Definition
+open Translation
+open TestHelpers
+
+[<TestFixture>]
+type TestGeneratedMatches() =
+
+    let baseText = "generated"
+
+    [<Test>]
+    member this.substitutesTypedVars() =
+        let generated = baseText + "0"
+        let source = ExFn (ExLambda([ExXPat "x", Some ExInt], ExX "x"))
+        let expected = Fn(Lambda(generated, Match (X generated, [Pat (XPat "x", Some Int), None, X "x"])))
+
+        let result = translate source emptyTransEnv
+
+        result |> should equal expected
+
+    [<Test>]
+    member this.substitutesMultiple() =
+        let generated1 = baseText + "0"
+        let generated0 = baseText + "1"
+        let source = ExFn (ExLambda([ExXPat "x", Some ExInt; ExXPat "y", None], ExX "x"))
+
+        let tupled = App (App (Constructor (Tuple 2), X generated0), X generated1)
+        let tupledPat = Pat (ConstructorPat (Tuple 2, [Pat (XPat "x", Some Int); Pat (XPat "y", None)]), None)
+        let inner = Fn(Lambda(generated1, Match (tupled, [tupledPat, None, X "x"])))
+        let expected = Fn (Lambda(generated0, inner))
+
+        let result = translate source emptyTransEnv
+
+        result |> should equal expected
+
+    [<Test>]
+    member this.keepsUntypedVars() =
+        let source = ExFn (ExLambda([ExXPat "x", None], ExX "x"))
+        let expected = Fn(Lambda("x", X "x"))
+
+        let result = translate source emptyTransEnv
+
+        result |> should equal expected
+
+    [<Test>]
+    member this.keepsMultipleUntyped() =
+        let source = ExFn (ExLambda([ExXPat "x", None; ExXPat "y", None], ExX "x"))
+        
+        let inner = Fn(Lambda("y", X "x"))
+        let expected = Fn (Lambda("x", inner))
+
+        let result = translate source emptyTransEnv
+
+        result |> should equal expected
